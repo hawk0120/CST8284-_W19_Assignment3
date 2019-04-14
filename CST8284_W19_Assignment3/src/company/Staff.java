175b0e16ef0fe2430784896f0c7be290075e90c4
Brady Hawkins <35853045+hawk0120@users.noreply.github.com>
2019-04-14 16:27:26 -0400

    Update Staff.java

--- CST8284_W19_Assignment3/src/company/Staff.java
+++ CST8284_W19_Assignment3/src/company/Staff.java
@@ -1,26 +1,39 @@
-/*Course Name: CST8284
- * Student Name: Brady Hawkins
- * Assignment title: CST8284_W19_Assign02_EmployementManagementSystem
- * Assignment due date: March. 31, 2019
- * 
- * 
- * Represents a Staff
+/**Staff.java
+ * @author BradyHawkins
+ * @version 1.0
+ * Course Name: CST8284
+ * Assignment title: CST8284_W19_Assign03_EmployementManagementSystem
+ * Assignment due date: April. 14, 2019
+ *
+ *Represents an abstraction of a Staff
  */
-
 package company;
 
 import java.util.Scanner;
 
 public class Staff extends Employee {
-	
+	/**
+	 * Field created to hold a string that represents the department of the Employee
+	 */
 	private String department;
 	
-	//Constructor
+	/**
+	 * Constructor
+	 * Instantiates it with initial values
+	 */
 	public Staff() {
 		this("Unknown", 5, new OurDate(30, 03, 2019), 4300.0, "Finance");
 	}
 	
-	//overloaded constructor, supers up to parent class to get params
+	/**
+	 * Base constructor
+	 * Supers up to parent class to get params
+	 * @param name
+	 * @param employeeNumber
+	 * @param startDate
+	 * @param salary
+	 * @param department
+	 */
 	public Staff(String name, int employeeNumber, OurDate startDate, double salary, String department) {
 		super(name,
 			employeeNumber,
@@ -29,15 +42,26 @@ public class Staff extends Employee {
 		this.setDepartment(department);
 	}
 	
-	//returns a string that represents the department
+	/**
+	 * Returns a string that represents the department
+	 * @return String
+	 */
 	public String getDepartment() {
 		return department;
 	}
-	//passes the department variable to the constructor
+	/**
+	 * Passes the department variable to the constructor
+	 * @param department
+	 * @return void
+	 */
 	public void setDepartment(String department) {
 		this.department = department;
 	}
-	//Overriden from parent class
+	/**
+	 * Overriden from parent class
+	 * Creates a new scanner object that gets a string input and passes it to department
+	 * @return void
+	 */
 	@Override
 	public void loadExtraInfo() {
 		Scanner sc = new Scanner(System.in); //Creates a new scanner obj
@@ -45,12 +69,18 @@ public class Staff extends Employee {
 		String department = sc.nextLine(); //asks for the department of the employee
 		this.setDepartment(department); //passes that employee's department to the setter
 	}
-	//returns a nicely formatted set of Strings to pass to parent classes
+	/**
+	 * Returns a  formatted set of Strings to pass to parent classes
+	 * @return String
+	 */
 	@Override
 	public String toString() {
 		return super.toString() + "\t " + department;
 	}
-	//returns a nicely formatted set of Strings to pass to parent classes
+	/**
+	 * Compares to objects
+	 * @return boolean
+	 */
 	@Override	
 	public boolean equals(Object obj) {
 		if ((obj == null) || (this.getClass() != obj.getClass())) return false;
