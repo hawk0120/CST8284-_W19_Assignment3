92337e25d72d38fd695fb6e74347f5b3d0a6444f
Brady Hawkins <35853045+hawk0120@users.noreply.github.com>
2019-04-14 16:26:50 -0400

    Update OurDate.java

--- CST8284_W19_Assignment3/src/company/OurDate.java
+++ CST8284_W19_Assignment3/src/company/OurDate.java
@@ -1,8 +1,10 @@
-/*Course Name: CST8284
- * Student Name: Brady Hawkins
+/**OurDate.java
+ * @author BradyHawkins
+ * @version 1.0
+ * 
+ * Course Name: CST8284
  * Assignment title: CST8284_W19_Assign03_EmployementManagementSystem
  * Assignment due date: April. 14, 2019
- * 
  * 
  * Represents a date
  */
@@ -16,91 +18,134 @@ import java.util.InputMismatchException;
 
 
 public class OurDate implements java.io.Serializable {
-
+	/**
+	 * Creates an obj of Calendar to get a default value for each field created
+	 */
 	private static final Calendar CAL = Calendar.getInstance();
+	/**
+	 * Fields created to hold instance of passed int, representing day, month, year
+	 * 
+	 */
 	private int day;
 	private int month;
 	private int year;
 
-	//Constructor
+	/**
+	 * Constructor
+	 * <p>
+	 * sets the default fields of this class to the current day, month, year
+	 */
 	public OurDate() {
 		this.day = CAL.get(Calendar.DATE);
 		this.month = CAL.get(Calendar.MONTH);
 		this.year = CAL.get(Calendar.YEAR);
 	} 
 
-	/// base constructor. Note input format: day, month, year, NOT year, month, day
-	// as used in Calendar constructor
+	/**
+	 * Base constructor.
+	 * @param day
+	 * @param month
+	 * @param year
+	 */
 	public OurDate(int day, int month, int year) {
 		this.setDay(day);
 		this.setMonth(month);
 		this.setYear(year);
 	} 
 
-	//returns an int that represents the day of the month
+	/**
+	 * Returns int that represents the day of the month
+	 * @return int
+	 */
 	public int getDay() {
 		return day;
 	}
 
-	//passes the day variable to the constructor of the class
+	/**
+	 * Passes Day variable to the Constructor to save in memory
+	 * @return void
+	 * @param day
+	 */
 	private void setDay(int day) {
 		this.day = day;
 	}
 
-	//returns an int that represents the month 
+	/**
+	 * returns an int that represents the month 
+	 * @return int
+	 */
 	public int getMonth() {
 		return month;
 	}
 
-	//passes the month variable to the constructor
+	/**
+	 * passes the month variable to the constructor
+	 * @param month
+	 * @return void
+	 */
 	private void setMonth(int month) {
 		this.month = month;
 	}
 
-	//returns an int that represents the year
+	/**
+	 * returns an int that represents the year
+	 * @return int
+	 */
 	public int getYear() {
 		return year;
 	}
 
-	//passes the year variable to the constructor
+	/**
+	 * Passes the year variable to the constructor
+	 * @param year
+	 * @return void
+	 */
 	private void setYear(int year) {
 		this.year = year;
 	}
-	//returns a nicely formatted set of Strings to pass to parent classes
+	/**
+	 * returns formatted set of Strings to pass to parent classes
+	 * @return String
+	 * @Override
+	 */
 	@Override	
 	public String toString() {
 		return day + "/" + month + "/" + year;
 	}
-
-	public boolean setOurDate(String s) {
+	/**
+	 * Checks to ensure the date format is appropriate
+	 * <p>
+	 * Passes a string. Parses that string, and checks to see if it's impossible.
+	 * @param s
+	 * @return boolean
+	 * @throw InputMismatchException
+	 */
+	public boolean setOurDate(String str) {
 		boolean result = true;
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 		dateFormat.setLenient(false);
 		try {
-			String[] dmy = s.split("/");
+			String[] dmy = str.split("/");
 			setYear(Integer.parseInt(dmy[2]));
 			setMonth(Integer.parseInt(dmy[1]));
 			setDay(Integer.parseInt(dmy[0]));
-			CAL.setTime(dateFormat.parse(s));  // use calendar to check for impossible dates
+			CAL.setTime(dateFormat.parse(str));  // use calendar to check for impossible dates
 			return result;
 		} catch (NumberFormatException g) {
-			throw new InputMismatchException("Bad input value; date " + s + " contains non-numeric value");
+			throw new InputMismatchException("Bad input value; date " + str + " contains non-numeric value");
 		} catch (IllegalArgumentException f) {
 			throw new InputMismatchException("Bad data type passed to an internal method");
 		}  catch (ParseException h) {
-			throw new InputMismatchException("Date " + s + " not possible");
+			throw new InputMismatchException("Date " + str + " not possible");
 		} catch (RuntimeException e) {
 			throw new InputMismatchException("General Runtime exception thrown setting start date");
 		} 
 	}
-
-
-
-
-	//returns a nicely formatted set of Strings to pass to parent classes
+	/**
+	 * Compares Obj passed to see if it's an instance of this class
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		return obj instanceof OurDate;
 	}	
-
-} //End of class
+} 
